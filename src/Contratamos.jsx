895c009ddee256bcba857809ae3a88c3891a2ad1
JuanDiegoAvila <60203575+JuanDiegoAvila@users.noreply.github.com>
2022-05-06 16:57:35 -0600

    Contratamos container

--- src/Contratamos.jsx
+++ src/Contratamos.jsx
@@ -1,15 +1,23 @@
 import React from 'react'
 
 import './styles/Contratamos.css'
+import Container5 from '../public/img/container-5.jpg'
 
 const Contratamos = () => (
   <div className="contratamos-container">
     <div className="contratamos-text">
-      adfsd
+      <h1>
+        Juntos, fortalecemos la creatividad en tiempo real por todo el mundo
+      </h1>
+      <p>
+        Únete al equipo global detrás de la plataforma de
+        desarrollo en tiempo real más poderosa del mundo.
+      </p>
+      <div className="cotratamos-button-container">
+        <button type="button" className="contratamos-button">Contratamos</button>
+      </div>
     </div>
-    <div className="contratamos-imagenes">
-      asdf
-    </div>
+    <div className="contratamos-imagenes" style={{ backgroundImage: `url(${Container5})` }} />
   </div>
 )
 
